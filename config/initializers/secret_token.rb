f41bb131bdf368b648428e0a5f39f34d78c348be
Marty <marty@ubuntu.(none)>
2013-08-21 22:14:37 -0700

    first initial git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ICoffeeWeb::Application.config.secret_token = '147f6ca1b446c764139b3c42b269b6586ad42ca2d7f5aadf0c427c81c9e5b5dfe4231e6d38a30aef992f60d8ae734656d20b1b54e7b9a95910b725ccea174b9d'
